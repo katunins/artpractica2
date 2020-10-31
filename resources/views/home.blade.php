1f2edbed9c14eb7fbf27a9e4118f6180e2044189
Pavel Katunin <katunin.pavel@gmail.com>
2020-11-01 00:15:02 +0300

    f

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Студия дизайна Artpractica</title>
+    <link rel="icon" href="{{ URL::asset('images/favicon.ico') }}" type="image/x-icon" />
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
